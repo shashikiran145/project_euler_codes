4922cc61ae93bca03319f811d04fa93d6d0f0b08
shashikiran145 <sshashikiransharma@gmail.com>
2025-08-05 01:50:15 +0200

    Added few more problems

--- problem_17.py
+++ problem_17.py
@@ -24,78 +24,78 @@ tens = {
     10: "ten",
     20: "twenty",
     30: "thirty",
-    40: "fourty",
+    40: "forty",
     50: "fifty",
     60: "sixty",
     70: "seventy",
     80: "eighty",
     90: "ninety",
 }
+
+hundreds = {
+    100: "onehundred",
+    200: "twohundred",
+    300: "threehundred",
+    400: "fourhundred",
+    500: "fivehundred",
+    600: "sixhundred",
+    700: "sevenhundred",
+    800: "eighthundred",
+    900: "ninehundred",
+}
 scales= ["hundred and", "thousand"]
 
 
-def two_digit(number):
-    elif len(str(n)) == 2:
-    if n in tens:
-        count.append(len(tens[n]))
-        print(tens[n])
-    elif n in teens:
-        count.append(len(teens[n]))
-        print(teens[n])
-    else:
-        quotient = n // 10
-        remainder = n % 10
-        quotient = quotient * 10
-        print(tens[quotient])
-        print(single_digits[remainder])
-        count.append(len(single_digits[remainder]))
-        count.append(len(tens[quotient]))
+def two_digit(n):
+    count = 0
+    quotient = n // 10
+    remainder = n % 10
+    quotient = quotient * 10
+    print(tens[quotient])
+    print(single_digits[remainder])
+    count += len(single_digits[remainder])
+    count += len(tens[quotient])
+    return count
 def number():
     count = []
     n = 1
     while n < 1000:
-        if len(str(n)) == 1:
-            remainder = n % 10
-            print(single_digits[remainder])
-            count.append(len(single_digits[remainder]))
-        elif len(str(n)) == 2:
-            if n in tens:
-                count.append(len(tens[n]))
-                print(tens[n])
-            elif n in teens:
-                count.append(len(teens[n]))
-                print(teens[n])
+        if n in single_digits:
+            count.append(len(single_digits[n]))
+            print(single_digits[n])
+        elif n in tens:
+            count.append(len(tens[n]))
+            print(tens[n])
+        elif n in teens:
+            count.append(len(teens[n]))
+            print(teens[n])
+        elif len(str(n)) == 2 and n not in tens and n not in teens:
+            count.append(two_digit(n))
+        elif len(str(n)) == 3:
+            if n in hundreds:
+                count.append(len(hundreds[n]))
             else:
-                quotient = n // 10
-                remainder = n % 10
-                quotient = quotient*10
-                print(tens[quotient])
-                print(single_digits[remainder])
-                count.append(len(single_digits[remainder]))
-                count.append(len(tens[quotient]))
-        elif len(str(n)) == 3 and n < 105:
-            if n in tens:
-                count.append(len(tens[n]))
-                print(tens[n])
-            elif n in teens:
-                count.append(len(teens[n]))
-                print(teens[n])
-            else:
-                quotient = n // 10
-                remainder = n % 10
-                quotient = quotient*10
-                print(tens[quotient])
-                print(single_digits[remainder])
-                count.append(len(single_digits[remainder]))
-                count.append(len(tens[quotient]))
-        else:
-            break
+                quotient = n // 100
+                remainder = n % 100
+                quotient = quotient * 100
+                count.append(len(hundreds[quotient])+3)
+                if remainder in single_digits:
+                    count.append(len(single_digits[remainder]))
+                    print(single_digits[remainder])
+                elif remainder in tens:
+                    count.append(len(tens[remainder]))
+                    print(tens[remainder])
+                elif remainder in teens:
+                    count.append(len(teens[remainder]))
+                    print(teens[remainder])
+                elif len(str(remainder)) == 2 and remainder not in tens and remainder not in teens:
+                    count.append(two_digit(remainder))
 
         n += 1
     print(count)
-    #print(sum(count))
+    print(sum(count))
 
 
 number()
 
-
+# Manually added for n = 1000
