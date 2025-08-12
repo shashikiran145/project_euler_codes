fe353fb0131c129e8efa58ff7c1b5ba4413f537f
shashikiran145 <sshashikiransharma@gmail.com>
2025-08-12 18:22:46 +0200

    Added few more problems

--- problem_21.py
+++ problem_21.py
@@ -1,26 +1,34 @@
-def ami(num):
+def divide(number):
     n = 1
     arr = []
-    while n <= (num/2):
-        if num % n == 0:
+    while n <= (number/2):
+        if number % n == 0:
             arr.append(n)
         n += 1
-    #print(arr)
     return sum(arr)
 
-# def main(number):
-#     n = 0
-#     sum_arr = []
-#     while n <= number:
-#         if ami(n) == number:
-#             sum_arr.append(number)
-#         else:
-#             print("Numbers not found")
-#         n += 1
-#     print(sum_arr)
 
-print(ami(10))
-#main(284)
+def main(number):
+    n = 0
+    diction = {}
+    addo = []
+    while n <= number:
+        diction[n] = divide(n)
+        n += 1
+    for a, b in diction.items():
+        if b in diction and diction[b] == a and a != b:
+            print(a, b)
+            if a+b in addo:
+                pass
+            else:
+                addo.append(a+b)
+
+    print(sum(addo))
+
+
+main(10000)
+
+
 
 
 
